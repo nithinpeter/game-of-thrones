6329a6705383fa34dcc86d6261f073061298e2f5
Nithin Peter <nithinpeter7@gmail.com>
2016-09-03 15:48:48 +1000

    Fix for heroku deployment issue.

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import env from "../helpers/env";
 
 const store = createStore(
     rootReducer,
-    env.isDev() ? window.devToolsExtension && window.devToolsExtension() : null
+    // env.isDev() ? window.devToolsExtension && window.devToolsExtension() : null
 );
 // applyMiddleware(thunkMiddleware, callApiMiddleware)ß
 
--- webpack.config.js
+++ webpack.config.js
@@ -39,12 +39,20 @@ const Config = {
         ]
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress:{
+                warnings: true
+            }
+        }),
         new CopyWebpackPlugin([
             {from: path.join(__dirname, './src/assets'), to: path.join(__dirname, './build/assets')}
         ]),
         new HtmlWebpackPlugin({
             template: path.join(__dirname, './src/template.ejs'),
-        })
+        }),
     ],
     devtool: "source-map"
 }
