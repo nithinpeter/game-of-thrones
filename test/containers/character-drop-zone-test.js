ebb4c7b663ef1c29f382ce523cbfa8f7b9571987
Nithin Peter <peter@jugglestreet.com>
2016-09-08 16:59:50 +1000

    Fix for failing character drop zone test.

--- test/containers/character-drop-zone-test.js
+++ test/containers/character-drop-zone-test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { expect } from "chai";
 import { shallow, mount, render } from "enzyme";
-import { CharactersDropZoneComponent } from "../../src/containers/characters-drop-zone";
+import { CharactersDropZone } from "../../src/containers/characters-drop-zone";
 import CharacterHolder from "../../src/components/character-holder";
 import wrapInTestContext from "../helpers/wrapInTestContext";
 import GameData from "../helpers/getTestData";
@@ -9,7 +9,19 @@ import GameData from "../helpers/getTestData";
 describe("Characters Drop Zone Component", function() {
 
   it("should render all character holders", function() {
+
+    const CharactersDropZoneComponent = wrapInTestContext(CharactersDropZone);
+
     const wrapper = mount(<CharactersDropZoneComponent isLoading={false} isFinished={false} gameData={GameData.characterTree}/>);
     expect(wrapper.find(CharacterHolder)).to.have.length(6);
   });
+
+  it("should not render when loading", function() {
+
+    const CharactersDropZoneComponent = wrapInTestContext(CharactersDropZone);
+
+    const wrapper = mount(<CharactersDropZoneComponent isLoading={true} isFinished={false} gameData={GameData.characterTree}/>);
+    expect(wrapper.find(CharacterHolder)).to.be.not.present();
+  });
+
 });
